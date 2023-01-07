34cc014707856e3dd7c230b4bef0ccfd96d075ea
Javier Samper Arias <javier.samper.arias@nttdata.com>
2023-01-07 20:50:23 +0100

    improve script performance by reducing http requests with promises.all

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 require("dotenv").config();
 
 const { getNode, getSvgImageUrl, getIconContent } = require("./utils/api");
-const { error, chunk, timer } = require("./utils/utils");
+const { error } = require("./utils/utils");
 const [fs, route, argv] = [
     require("fs"),
     require("path"),
@@ -16,9 +16,9 @@ const icons = data[theme][key] || {};
 
 /**
  * @description This function gets the node and inserts it into the icon array
- * @param {string} item 
- * @param {string} name 
- * @param {Array<{name: string;node: string}>} _promises 
+ * @param {string} item
+ * @param {string} name
+ * @param {Array<{name: string;node: string}>} _promises
  */
 const getValueToken = (item, name, _promises) => {
     const { value } = item;
@@ -28,103 +28,71 @@ const getValueToken = (item, name, _promises) => {
         .slice(0, -1);
 
     if (!fs.existsSync(route.resolve(path, `${name}.svg`))) _promises.push({ name, nodeId });
-}
+};
 /**
  * @description This function returns an array with the name and node of each figma icon
  * @returns {Array<{name: string;node: string}>}
  */
-const awaitUrlIcons = () => {
-    return new Promise((resolve) => {
-        let _promises = [];
-
-        Object.entries(icons).forEach(async ([name, item], index) => {
-            if (Object.keys(item).every(key => key !== 'value')) {
-                Object.entries(item).forEach(async ([nameChild, child], index) => {
-                    getValueToken(child, `${name}_${nameChild}`, _promises);
-                })
-            } else {
-                getValueToken(item, name, _promises);
-            }
-            if (index === Object.keys(icons).length - 1) {
-                if (_promises.length === 0) {
-                    error('There are no new icons to import');
-                } else {
-                    resolve(_promises);
-                }
-            }
-        });
+const getNodeIcons = () => {
+    let nodes = [];
+    Object.entries(icons).forEach(([name, item]) => {
+        if (Object.keys(item).every((key) => key !== "value")) {
+            Object.entries(item).forEach(([nameChild, child]) => {
+                getValueToken(child, `${name}_${nameChild}`, nodes);
+            });
+        } else {
+            getValueToken(item, name, nodes);
+        }
     });
+
+    return nodes;
 };
+
 /**
- * this function returns the svg content of each icon and will create it as a file
- * @param {Array<{name: string;node: string}>} icons - Array chunk icons specefied
+ * @description This function downloads the content of the icons and creates the svg file
  */
-const getIconSvg = async (icons) => {
-    let isError = false;
-    for await (item of icons) {
-        const { name, nodeId } = item;
-        if(isError) break;
-        await getNode(nodeId)
+const getIcons = async () => {
+    const nodes = getNodeIcons();
+
+    if (!fs.existsSync(route.resolve(process.cwd(), path))) fs.mkdirSync(route.resolve(process.cwd(), path), { recursive: true });
+    if (nodes.length) {
+        await Promise.all(nodes.map(async ({ name, nodeId }) => ({ name, node: await getNode(nodeId) })))
             .then(async (response) => {
-                await getSvgImageUrl(response)
-                    .then(async (urlContent) => {
-                        await getIconContent(urlContent)
-                            .then((response) => {
-                                const { data } = response;
-                                fs.writeFile(route.resolve(path, `${name}.svg`), data, () =>
-                                    console.log(
-                                        `✅ The icon ${name}.svg has been successfully created in the path ${path}/${name}.svg`
-                                    )
-                                );
-                            })
-                            .catch((err) => {
-                                error(err);
-                            });
-                    })
-                    .catch(err => {
-                        error(err);
-                    })
+                const allSvgUrl = await Promise.all(response.map(async ({ name, node }) => ({ name, url: await getSvgImageUrl(node) })));
+                const allSvgContent = await Promise.all(allSvgUrl.map(async ({ name, url }) => {
+                    const { data } = await getIconContent(url);
+                    return { name, data };
+                })
+                );
+                for await ([index, icon] of allSvgContent.entries()) {
+                    const { name, data } = icon;
+                    fs.writeFile(route.resolve(path, `${name}.svg`), data, () =>
+                        console.log(
+                            `✅ The icon ${name}.svg has been successfully created in the path ${path}/${name}.svg`
+                        )
+                    );
+                }
             })
             .catch((err) => {
-                const { data: { status } } = err.response;
+                const {
+                    data: { status },
+                } = err.response;
 
                 if (status === 403) {
-                    error('Check figma authorization token');
+                    error("Check figma authorization token");
                     isError = true;
                 } else {
                     error(err.response.data);
                 }
-            })
+            });
+    } else {
+        error("There are no new icons to import");
     }
-};
-
-/**
- * @description This function initializes the import of figma icons
- */
-const getIcons = async () => {
-    if (!fs.existsSync(route.resolve(process.cwd(), path)))
-        fs.mkdirSync(route.resolve(process.cwd(), path), { recursive: true });
-
-    awaitUrlIcons()
-        .then(async (promises) => {
-            const chunks = chunk(promises, 10);
-            for await (item of chunks) {
-                getIconSvg(item);
-                if (chunks.length > 1) await timer(45000);
-            }
-        })
-        .catch((err) => {
-            error(err);
-        });
 };
 
 if (isIcons) {
     if (Object.values(icons).length) {
-        if (process.env.FIGMA_TOKEN && process.env.FILE_KEY) {
-            getIcons();
-        } else {
-            error('Check environment variables');
-        }
+        process.env.FIGMA_TOKEN && process.env.FILE_KEY ? getIcons() : error("Check environment variables");
     } else {
         error(`Check the script flags`);
     }
--- utils/utils.js
+++ utils/utils.js
@@ -6,29 +6,7 @@ let { red } = require('colors');
  * @returns {String}
  */
 const error = (message) => console.log(red(message));
-/**
- * Timer is a function that returns a promise that resolves after a given number of milliseconds.
- * @param ms - The number of milliseconds to wait before resolving the promise.
- * @returns {Promise<setTimeout>}
- */
-const timer = (ms) => new Promise((res) => setTimeout(res, ms));
-
-
-/**
- * If the index of the current element is divisible by the size, then push a new chunk into the
- * accumulator.
- * @param arr - The array to be chunked
- * @param size - The size of each chunk
- * @returns {Array<{name: string;node: string}[]>}
- */
-const chunk = (arr, size) =>
-    arr.reduce((acc, _, i) => {
-        if (i % size === 0) acc.push(arr.slice(i, i + size));
-        return acc;
-    }, []);
 
 module.exports = {
-    error,
-    timer,
-    chunk,
+    error
 };
